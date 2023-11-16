03b191a00ba4b93739936acbc624d28dd5528a67
psi5059 <psi5059@naver.com>
2023-11-16 13:21:11 +0900

    first commit

---
+++ src/main/java/com/blueprintto3d/Blueprintto3dApplication.java
@@ -0,0 +1,13 @@
+package com.blueprintto3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blueprintto3dApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blueprintto3dApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blueprintto3d/Blueprintto3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueprintto3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blueprintto3dApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
