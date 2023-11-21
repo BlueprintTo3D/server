d308a6290c07f92402622ae1480aacbcdd09e46f
psi5059 <psi5059@naver.com>
2023-11-22 08:49:14 +0900

    [Bugfix] canvas 배경 색상 변경

--- src/main/resources/static/babylon/main.js
+++ src/main/resources/static/babylon/main.js
@@ -3,24 +3,34 @@ window.addEventListener('DOMContentLoaded', function () {
     var engine = new BABYLON.Engine(canvas, true);
 
     BABYLON.SceneLoader.Load("babylon/", "floorplan56_s0.5_h3_brick_4k.gltf", engine, function (newScene) {
-        var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), newScene);
-        camera.attachControl(canvas, true);
-        newScene.activeCamera = camera;
+            var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), newScene);
 
-        newScene.createDefaultLight();
+            // camera.inputs.removeByType("ArcRotateCameraKeyboardMoveInput");
 
-        engine.runRenderLoop(function () {
-            newScene.render();
-        });
+            camera.attachControl(canvas, true);
+            newScene.activeCamera = camera;
 
-        // SceneOptimizer를 이용한 성능 최적화
-        var options = new BABYLON.SceneOptimizerOptions();
-        options.addOptimization(new BABYLON.HardwareScalingOptimization(0, 1));
+            newScene.createDefaultLight();
 
-        var optimizer = new BABYLON.SceneOptimizerOptions(newScene, options);
-        optimizer.start();
+            // 캔버스 배경 색상 변경
+            newScene.clearColor = new BABYLON.Color3(20/255, 20/255, 20/255);
 
-    }, function (progress) {
-        console.log((progress.loaded / progress.total * 100) + "% loaded");
-    });
+            engine.runRenderLoop(function () {
+                newScene.render();
+            });
+
+            // SceneOptimizer를 이용한 성능 최적화
+            var options = new BABYLON.SceneOptimizerOptions();
+            options.addOptimization(new BABYLON.HardwareScalingOptimization(0, 1));
+
+            var optimizer = new BABYLON.SceneOptimizerOptions(newScene, options);
+            optimizer.start();
+
+        }, function (progress) {
+            console.log((progress.loaded / progress.total * 100) + "% loaded");
+        }, function (scene, message, exception) {
+            console.error("scene 로딩 중 에러", message, exception);
+        }
+
+    );
 });
