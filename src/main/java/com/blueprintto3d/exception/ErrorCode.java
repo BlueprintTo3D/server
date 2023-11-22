bd4802c1a62759ee6e5ffcf26674d128ee0fe57a
psi5059 <psi5059@naver.com>
2023-11-22 10:27:50 +0900

    [Feat] Exception 처리

---
+++ src/main/java/com/blueprintto3d/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.blueprintto3d.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+
+
+
+    ;
+
+    private HttpStatus status;
+    private String message;
+}
